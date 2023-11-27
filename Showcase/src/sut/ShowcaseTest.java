aabb098d345049630621c4787803bdb06477e6ca
LawrenceT02 <lt6vc@virginia.edu>
2023-11-27 00:23:02 -0500

    Add to cart from gear test

--- Showcase/src/sut/ShowcaseTest.java
+++ Showcase/src/sut/ShowcaseTest.java
@@ -16,6 +16,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 
 class ShowcaseTest {
@@ -231,37 +234,63 @@ class ShowcaseTest {
 	
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-////////////////////////////////////////////// Add to Cart From What's New ///////////////////////////////////////////////////////
+////////////////////////////////////////////// Add to Cart From Gear ///////////////////////////////////////////////////////
 //	//lt6vc
-//	@Test								// Can't get selenium to click on size or color button
-//	void baseCaseAddtoCartTest() {
+//	@Test
+//	void GearClckItemSelPosQtyAddCartTest() {
 //		JavascriptExecutor js = (JavascriptExecutor) driver;
-//		driver.findElement(By.linkText("What's New")).click();
-//		driver.findElement(By.xpath("/html/body/div[2]/main/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/strong/a")).click(); // xpath for Phoebe Zipper Sweatshirt
-//		js.executeScript("document.getElementById('option-label-size-143-item-167').click();");
-////		driver.findElement(By.id("option-label-size-143-item-167")).click(); // xpath for small (CANT FIND ELEMENT)
-////		driver.findElement(By.id("option-label-color-93-item-52")).click();
+//		driver.findElement(By.linkText("Gear")).click();
+//		driver.findElement(By.xpath("/html/body/div[2]/main/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/strong/a")).click(); // xpath for water bottle
+//		driver.navigate().refresh();
 //		driver.findElement(By.id("qty")).clear();
 //		driver.findElement(By.id("qty")).sendKeys("1");
-//		WebElement element = driver.findElement(By.id("product-addtocart-button"));
-//		js.executeScript("arguments[0].click();", element);
+//		driver.findElement(By.id("qty")).sendKeys(Keys.TAB, Keys.RETURN);
+//		try {
+//            Thread.sleep(1500); // 5000 milliseconds = 5 seconds
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 //		driver.get("https://magento.softwaretestingboard.com/checkout/cart/");
-//		driver.getPageSource().contains("Phoebe Zipper Sweatshirt");
+//		assertTrue(driver.getPageSource().contains("Affirm Water Bottle"));
 //	}
 	
-	//lt6vc
-	@Test
-	void tabClickedItemSelectedNoSizeGrayCartClickedTest() {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		driver.findElement(By.linkText("What's New")).click();
-		driver.findElement(By.xpath("/html/body/div[2]/main/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[1]/div/div/strong/a")).click(); // xpath for Phoebe Zipper Sweatshirt
-		driver.findElement(By.id("qty")).clear();
-		driver.findElement(By.id("qty")).sendKeys("1");
-		WebElement element = driver.findElement(By.id("product-addtocart-button"));
-		js.executeScript("arguments[0].click();", element);
-		js.executeScript("arguments[0].click();", element);
-		element.click();
-	}
+//	//lt6vc
+//	@Test
+//	void GearClckItemSelNegQtyAddCartTest() {
+//		driver.findElement(By.linkText("Gear")).click();
+//		driver.findElement(By.xpath("/html/body/div[2]/main/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/strong/a")).click(); // xpath for water bottle
+//		driver.navigate().refresh();
+//		driver.findElement(By.id("qty")).clear();
+//		driver.findElement(By.id("qty")).sendKeys("-1");
+//		driver.findElement(By.id("qty")).sendKeys(Keys.TAB, Keys.RETURN);
+//		try {
+//            Thread.sleep(1000); // 5000 milliseconds = 5 seconds
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//		driver.get("https://magento.softwaretestingboard.com/checkout/cart/");
+//		assertFalse(driver.getPageSource().contains("Affirm Water Bottle"));
+//		assertTrue(driver.getPageSource().contains("You have no items in your shopping cart"));
+//	}
+	
+//	//lt6vc
+//	@Test
+//	void GearClckItemSelZeroQtyAddCartTest() {
+//		driver.findElement(By.linkText("Gear")).click();
+//		driver.findElement(By.xpath("/html/body/div[2]/main/div[4]/div[1]/div[1]/div[3]/div/div/ol/li[3]/div/div/strong/a")).click(); // xpath for water bottle
+//		driver.navigate().refresh();
+//		driver.findElement(By.id("qty")).clear();
+//		driver.findElement(By.id("qty")).sendKeys("0");
+//		driver.findElement(By.id("qty")).sendKeys(Keys.TAB, Keys.RETURN);
+//		try {
+//            Thread.sleep(1000); // 5000 milliseconds = 5 seconds
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//		driver.get("https://magento.softwaretestingboard.com/checkout/cart/");
+//		assertFalse(driver.getPageSource().contains("Affirm Water Bottle"));
+//		assertTrue(driver.getPageSource().contains("You have no items in your shopping cart"));
+//	}
 	
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
